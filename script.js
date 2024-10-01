229569b3e6184146979ee34f1b03f44992fd9fc6
joseldsm <165505426+joseldsm@users.noreply.github.com>
2024-10-01 16:23:11 +0200

    Style and layout improved and valid/invalid detection coded.

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-let form = document.getElementById("form");
+let form = document.querySelector("form");
+let div = document.createElement("div");
 
 const inputs = [
-    {type: "text", name: "first-name", text: "Prénom", pattern: ""},
-    {type: "text", name: "last-name", text: "Nom", pattern: ""},
-    {type: "email", name: "email", text: "Email", pattern: ""},
-    {type: "text", name: "phone-number", text: "Numéro de téléphone", pattern: "\d{2}\s\d{2}\s\d{2}\s\d{2}\s\d{2}"},
+    {type: "text", name: "first-name", text: "Prénom", placeholder: "José", pattern: ""},
+    {type: "text", name: "last-name", text: "Nom", placeholder: "Lopes", pattern: ""},
+    {type: "email", name: "email", text: "Email", placeholder: "ahahah@blablabla.com", pattern: ""},
+    {type: "text", name: "phone-number", text: "Numéro de téléphone", placeholder: "01 02 03 04 05", pattern: "^\\d{2}\\s\\d{2}\\s\\d{2}\\s\\d{2}\\s\\d{2}$"},
     {type: "password", name: "password", text: "Mot de passe", pattern: ""},
     {type: "password", name: "confirm-password", text: "Confirmation mot de passe", pattern: ""},
 ]
 
 inputs.forEach(input => {
-    let div = document.createElement("div");
     const newInput = document.createElement("input");
-    const newLabel = document.createElement("label");
+    const checkMarker = document.createElement("span");
+    let newLabel = document.createElement("label");
+    let divInline = document.createElement("div");
+
+    checkMarker.textContent = "\u25CF";
 
     newInput.setAttribute("type", input.type);
     newInput.setAttribute("name", input.name);
@@ -22,17 +26,21 @@ inputs.forEach(input => {
     }
     newInput.setAttribute("required", "required");
 
+    if (input.type !== "password") {
+        newInput.setAttribute("placeholder", input.placeholder);
+    }
+
     newLabel.setAttribute("for", input.name);
     newLabel.textContent = input.text;
 
+    divInline.appendChild(newInput);
+    divInline.appendChild(checkMarker);
+    newLabel.appendChild(divInline);
     div.appendChild(newLabel);
-    div.appendChild(newInput);
-    form.appendChild(div);
 })
-
-
 const submit = document.createElement("button");
 submit.setAttribute("type", "submit");
 submit.textContent = "Submit";
 
+form.appendChild(div);
 form.appendChild(submit);
